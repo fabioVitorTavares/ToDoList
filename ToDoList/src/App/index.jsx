9ea0e10b5bd913bd771b8fa2ff78134e01bb4b55
fabioVitorTavares <fabio.furtado@estudante.ufla.br>
2022-11-09 23:38:26 -0300

    initial react.js project

---
+++ ToDoList/src/App/index.jsx
@@ -0,0 +1,11 @@
+import { Task } from  '../Task'
+
+export function App() {
+  return (
+    <div className="App">
+      <h1>Olá, Meu nome é Fábio!</h1>
+      <Task />      
+    </div>
+  )
+}
+
---
+++ ToDoList/src/Task/index.jsx
@@ -0,0 +1,13 @@
+
+export function Task(){
+    return (
+        <details>
+            <summary>Tarefas</summary>
+            <ul>
+                <li>Tomar café</li>
+                <li>Codar</li>
+                <li>Almoçar</li>
+            </ul>
+        </details>
+    )
+}
