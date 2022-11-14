e8d43d0022caf3facd27c7e564ada63f3e67fb87
fabioVitorTavares <fabio.furtado@estudante.ufla.br>
2022-11-13 22:36:39 -0300

    add components in project

---
+++ ToDoList/src/Tasks/index.jsx
@@ -0,0 +1,15 @@
+import { Task } from '../Task'
+import './style.css';
+
+export function Tasks(){
+    return (
+        <div className='tasks'>
+            <Task description="Tomar café"/>      
+            <Task description="Estudar styleds components"/>      
+            <Task description="Codar/praticar"/>      
+            <Task description="Almoçar"/>      
+            <Task description="Estudar Java"/>      
+            <Task description="Academia"/> 
+        </div>
+    )
+}
