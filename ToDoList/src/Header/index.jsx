c116e1b0d5d582c7cbb6b138d3b90923528790d0
fabioVitorTavares <fabio.furtado@estudante.ufla.br>
2022-11-15 11:07:34 -0300

    add header

---
+++ ToDoList/src/Header/index.jsx
@@ -0,0 +1,13 @@
+import './style.css'
+
+export function Header() {
+  return (
+    <nav className='header'>
+      <div>
+        <p>Tasks</p>
+        <p>About</p>
+      </div>
+      <img src="https://github.com/fabiovitortavares.png" alt="" />
+    </nav>
+  )
+}
