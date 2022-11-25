aa2e638cbd6eb9d79ea7e3bee31254534024fa0c
fabioVitorTavares <fabio.furtado@estudante.ufla.br>
2022-11-25 11:33:37 -0300

    add minCalendar

--- ToDoList/src/components/Calendar/index.tsx
+++ ToDoList/src/components/Calendar/index.tsx
@@ -1,16 +1,15 @@
 import './style.css'
 import 'react-day-picker/dist/style.css';
 import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
-import { DayPicker } from 'react-day-picker';
+import { MinCalendar } from '../MinCalendar'
 
-type TSetDate = {
+export type TSetDate = {
   setDate: Dispatch<SetStateAction<Date>>
   date: Date
 }
 
 export function Calendar({ setDate, date }: TSetDate) {
   
-  const [selected, setSelected] = useState<Date>(new Date);
 
   const tomorrow = () => {
     const newDate = new Date
@@ -18,7 +17,6 @@ export function Calendar({ setDate, date }: TSetDate) {
     newDate.setMonth(date.getMonth())
     newDate.setDate(date.getDate() + 1)
     setDate(newDate)    
-    setSelected(newDate)
   }
 
   const yesterday = () => {
@@ -27,21 +25,14 @@ export function Calendar({ setDate, date }: TSetDate) {
     newDate.setMonth(date.getMonth())
     newDate.setDate(date.getDate() - 1)
     setDate(newDate)   
-    setSelected(newDate)
   }
 
-  useEffect(() => {
-    setDate(selected)    
-  }, [selected])
+
 
   return (
     <div className='calendar'>
-      <DayPicker
-        className='dateSelector'
-        mode="single"
-        selected={selected}
-        onSelect={()=>setSelected}        
-      />
+      
+      <MinCalendar setDate={setDate} date={date} />
       <div className='viewDay'>
         <button onClick={yesterday}>-</button>
         <h1>{date.toLocaleDateString()}</h1>
